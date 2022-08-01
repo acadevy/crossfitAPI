830a4eea0a23785db1813ce82070e2652a4d2ee4
acadevy <rashyolat@gmail.com>
2022-08-01 14:43:01 +0100

    added the workoutroutes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-// In src/index.js 
-const express = require("express"); 
+const express = require("express");
 
-const app = express(); 
-const PORT = process.env.PORT || 3000; 
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
-// For testing purposes 
-app.get("/", (req, res) => { 
-    res.send("<h2>It's Working!</h2>"); 
-}); 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
+
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
 });
-
