ad298d35bc9e9fa2876def044bbcd5351b44d3e2
acadevy <rashyolat@gmail.com>
2022-08-02 17:17:22 +0100

    documented the API

---
+++ src/v1/swagger.js
@@ -0,0 +1,47 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+// Basic Meta Informations about our API
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: { title: "Crossfit WOD API", version: "1.0.0" },
+  },
+  apis: ["./src/v1/routes/workoutRoutes.js", "./src/database/Workout.js"],
+};
+const workoutsOptions = {
+    definition: {
+      openapi: "3.0.0",
+      info: { title: "Crossfit WOD API", version: "1.0.0" },
+    },
+    apis: ["./src/v1/routes/workoutRoutes.js", "./src/database/Workout.js"],
+  };
+
+// Docs in JSON format
+const swaggerSpec = swaggerJSDoc(options);
+const workoutSpec = swaggerJSDoc(workoutsOptions);
+
+// Function to setup our docs
+const swaggerDocs = (app, port) => {
+  // Route-Handler to visit our docs
+  app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  // Make our docs in JSON format available
+  app.get("/api/v1/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+  console.log(
+    `Version 1 Docs are available on http://localhost:${port}/api/v1/docs`
+  );
+};
+const workoutDocs = (app, port) => {
+    // Route-Handler to visit our docs
+    app.use("/api/v1/docs/workouts", swaggerUi.serve, swaggerUi.setup(workoutSpec));
+    // Make our docs in JSON format available
+    app.get("/api/v1/docs.json", (req, res) => {
+      res.setHeader("Content-Type", "application/json");
+      res.send(workoutSpec);
+    })
+  };
+
+module.exports = { swaggerDocs,workoutDocs };
